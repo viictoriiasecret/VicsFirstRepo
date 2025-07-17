2cd431b58ce50b8aec4e053ebc61172af6af39a5
Victoria Ogunmakinwa <viictoriiasecret>
2025-07-17 13:15:58 -0700

    added two tests

---
+++ Testing/shoppingcart1.test.js
@@ -0,0 +1,15 @@
+const shoppingcart = require ('./behavior')
+const listings = {jacket: 6, shirt: 5, necklace: 4, jeans: 9}
+let quantity = {jacket: 3 , shirt: 4, necklace: 5, jeans: 7}  //quantity 
+
+
+test('makes sure theres enough to buy', () => {
+   
+        expect(selection[item] > 0).toBe(true)
+     // structure: expect(logical)function(truthy/falsy)
+})
+
+test('makes sure the object exists', () => {
+    
+    expect(item.keys(listings)).toContain(selection);
+  });
